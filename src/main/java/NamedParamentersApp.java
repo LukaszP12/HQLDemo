09b3b60f92562f24375386b05bfe8d0927c25b82
LukaszP12 <lukaszp4@onet.eu>
2020-04-19 21:32:39 +0200

    5th commit including the "NamedParametersApp"

---
+++ src/main/java/NamedParamentersApp.java
@@ -0,0 +1,35 @@
+import Entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+public class NamedParamentersApp {
+
+    public static void main(String[] args) {
+        Configuration conf = new Configuration().configure("hibernate.cfg.xml");
+
+        conf.addAnnotatedClass(Employee.class);
+
+        SessionFactory factory = conf.buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        session.beginTransaction();
+
+        String employeeFirstName = "Steven";
+        String employeeLastName = "King";
+
+        String namedParameterString = "select e.firstName, e.lastName,e.salary from Employee e where e.firstName=:firstName and e.lastName:=lastName";
+
+        Query namedParameterQuery = session.createQuery(namedParameterString);
+        namedParameterQuery.setParameter("firstName",employeeFirstName);
+        namedParameterQuery.setParameter("lastName",employeeLastName);
+
+        session.getTransaction().commit();
+
+        session.close();
+
+    }
+
+}
--- src/main/java/SelectApp.java
+++ src/main/java/SelectApp.java
@@ -20,12 +20,23 @@ public class SelectApp {
         session.beginTransaction();
 
         String select = "select firstName, lastName from Employee";
+        String select2 = "select e.firstName, e.lastName from Employee as e";
+        String select3 = "select e.firstName, e.lastName from Employee e";
+        String select4 = "select e.firstName,e.lastName from Employee e order by e.firstName"; // order by
+
         Query query = session.createQuery(select);
         List<Object> resultList = query.getResultList();
 
         for (Object result : resultList){
             System.out.println(result);
         }
+
+        Query query1 = session.createQuery(select2);
+        List<Object> resultList1 = query1.getResultList();
+
+        Query query2 = session.createQuery(select4);
+        List<Object> resultListOrderBy = query2.getResultList();
+
 
         session.getTransaction().commit();
 
